12b29f6c074c2e8326371aba57eb726bc9cad681
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 12:37:33 +0530

    adding comparison or relational operators

--- operators.py
+++ operators.py
@@ -11,3 +11,15 @@ print(a/b)
 print(a%b)
 print(a**b)
 
+"""relational /comparison Operators"""
+"(==, != ,<, >=, <=)"
+c=30
+d=50
+print(c==d)
+print(c!=d)
+print(c<=d)
+print(c>=d)
+print(c<d)
+print(c>d)
+
+
