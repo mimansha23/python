64ec0c667fe9bd15e83d6bb30b277456a96ff45c
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 12:24:41 +0530

    print operators of arithematics

---
+++ operators.py
@@ -0,0 +1,13 @@
+"""types of operators"""
+"""an operator is a symbol that performs a certain operation between operands"""
+#arithmetaics operators
+#like +,-,*,/,%(modulo),**(power operator) 
+a=5
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
