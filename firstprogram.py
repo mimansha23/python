4fbcc11d163372ef213f941700749481f477bc47
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 11:11:11 +0530

    adding variables

--- firstprogram.py
+++ firstprogram.py
@@ -2,3 +2,11 @@ print("Hello world")
 print(32)
 #print number
 print(21+22)
+#adding numbers
+#variable is a name given to a memory location in aprogram .
+name="mimansha "
+age = 19
+price =25.66
+print(name)
+print (age)
+print(price)
