619d67ab0147ad9c6afb5e69519d89011a14946a
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 11:23:16 +0530

    print a sentnce

--- firstprogram.py
+++ firstprogram.py
@@ -4,9 +4,18 @@ print(32)
 print(21+22)
 #adding numbers
 #variable is a name given to a memory location in aprogram .
-name="mimansha "
-age = 19
-price =25.66
+name="mimansha " #string
+age = 19 #number
+price =25.66 #floating value
 print(name)
 print (age)
 print(price)
+#here we print name in "" then its print the name and when we print 
+#name without "" then its print the variable
+# variable is consider as change 
+#here name is variable and mimansha is value 
+print("my name is :", name)
+print("my age is:", age)
+#print a sentance using variables
+
+
