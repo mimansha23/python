1fd39c3cd5b8b0a8297f34c0b415ce2cd6547b89
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 11:49:59 +0530

    print type of variable

--- firstprogram.py
+++ firstprogram.py
@@ -17,5 +17,14 @@ print(price)
 print("my name is :", name)
 print("my age is:", age)
 #print a sentance using variables
+# the = sign is called assingment tool which is used to assign values like they are asigning the values from left to right
+# like the value of age is 25 is assign (store ) in age variable
+#rules for identifier (variable name)
+# variables used are = uppercase lowercase digits underscore myvariable variable_1 etc
+# but variables that are not used are = !@#$%^&*()_ starts with symbols like that and from digits also
+# they are of any length but use simple short and meaningfull variables are smart choice
+print(type(name))
+print(type(age))
+print(type(price))
 
 
