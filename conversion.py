49af2c5244ffc1651d25c9ed5bc9852b7f972634
mimansha23 <mimanshapandey89@gmail.com>
2026-06-06 21:47:34 +0530

    type conversion and type casting

---
+++ conversion.py
@@ -0,0 +1,21 @@
+#type conversion
+"""a = "2"
+b = 4.25"""
+
+
+"""print(a + b)"""
+#there is a error which says that str can not concantenate float 
+
+
+
+"""then we type casting"""
+#like changing or converting variables
+#when we find error in the above type conversion then we use the term type casting for change the variable type so that there is no different errors find when we add or do calculations on different variables
+#like example
+
+a = float("2")
+b = 4.25
+
+print(type(a))
+print(a+b)
+
