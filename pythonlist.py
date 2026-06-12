86a983129d77ddd459f2d08ef512fd7923422f4a
mimansha23 <mimanshapandey89@gmail.com>
2026-06-12 20:46:57 +0530

    using adding list names

--- pythonlist.py
+++ pythonlist.py
@@ -1,6 +1,10 @@
 """thislist = ["apple", "banana", "cherry"]
 print(thislist)
 """
-thislist = ["apple", "banana", "cherry"]
+"""thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrant"
+print(thislist)"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
 print(thislist)
