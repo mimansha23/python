181f15b5fc9b6ceb911ec52845f46c31866fb7c8
mimansha23 <mimanshapandey89@gmail.com>
2026-06-14 22:14:57 +0530

    solve programming function of sum

--- practiceqst.py
+++ practiceqst.py
@@ -1,4 +1,10 @@
 #write a program to input 2 numbers & print their sum
-a=2;
+#i print the sum but we have to print their sum in terminal
+"""a=2;
 b=3;
-print(a+b)
+print(a+b)"""
+
+
+first = int(input("enter first number :" ))
+second = int(input("enter second number :" ))
+print ("sum=", first + second)
