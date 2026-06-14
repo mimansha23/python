ae409ab3e2899b717369b9a598cdb242b19a08ad
mimansha23 <mimanshapandey89@gmail.com>
2026-06-14 22:20:06 +0530

    solve side of square problem

--- practiceqst.py
+++ practiceqst.py
@@ -5,6 +5,21 @@ b=3;
 print(a+b)"""
 
 
-first = int(input("enter first number :" ))
+"""first = int(input("enter first number :" ))
 second = int(input("enter second number :" ))
 print ("sum=", first + second)
+
+
+#output
+
+enter first number :12
+enter second number :45
+sum= 57"""
+
+
+
+#Q2 #WAp to input side of a square & print its area
+
+side = float(input("enter square side: "))
+
+print("area =", side ** 2)
