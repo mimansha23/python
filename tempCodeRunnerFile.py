86d0c27cd258a8842e604afccbcce212716903c6
mimansha23 <mimanshapandey89@gmail.com>
2026-06-14 22:00:36 +0530

    print different types of variables by assingig them into different variables

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -13,5 +13,32 @@ print("welcome", name)"""
 str becuase it convert it because of type(val)"""
 #for example 
 
-val = input("enter some value : ")
+"""val = input("enter some value : ")
 print(type(val), val) #"25" , "99.99"
+#just like type casting"""
+
+"""name = input("enter name: ")
+age =input("enter age: ")
+marks = input("enter marks: ")
+
+print("welcome", name)
+print("age =" , age)
+print("marks =", marks)"""
+#enter name: mimu
+"""enter age: 19
+enter marks: 90
+welcome mimu
+age = 19
+marks = 90"""
+#so when we name = input("enter name: ")
+"""age =input("enter age: ")
+marks = input("enter marks: ")"""
+#then above output is seen and when we apply int float str for there name then its print 
+
+name = input("enter name: ")
+age =int(input("enter age: "))
+marks = float(input("enter marks: "))
+
+print("welcome", name)
+print("age =" , age)
+print("marks =", marks)
