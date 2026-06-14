653d888691075827ca8281d978ef447603e49906
mimansha23 <mimanshapandey89@gmail.com>
2026-06-14 21:46:56 +0530

    type casting values in another variable to print them same

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,17 @@
-name =input("enter your name: ")
-print("welcome", name)
+"""name =input("enter your name: ")
+print("welcome", name)"""
+#input in python
+#input()statement is used to accept values (using keyboards) from user
+
+#input() #result for output() is always a str
+
+#int(input()) #int
+
+#float(input()) #float
+
+"""when we print any number or name or decimal value than the val print the type 
+str becuase it convert it because of type(val)"""
+#for example 
+
+val = input("enter some value : ")
+print(type(val), val) #"25" , "99.99"
