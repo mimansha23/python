f8cec6ec8a13d01d9257c02a4bd4c6e66057d49b
mimansha23 <mimanshapandey89@gmail.com>
2026-06-16 22:24:39 +0530

    adding two strings

--- string.py
+++ string.py
@@ -3,3 +3,15 @@ str2='MIMANSHA PANDEY'
 str3="""this is me mimu"""
 
 """this is apna colleges tutorial"""
+"""escape sequence character 
+tab , \n   ,\t"""
+
+#string is data type that stores a sequence of characters
+#concatenation
+#plus operators
+#adding to strings
+
+str1 ="mimansha"
+str2 = "pandey"
+final_str =str1+str2
+print(final_str)
