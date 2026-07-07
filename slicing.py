1ef8b5b06a5cca60b528ea8bc1b42e4db266769f
mimansha23 <mimanshapandey89@gmail.com>
2026-07-07 22:29:54 +0530

    negative index slicing

--- slicing.py
+++ slicing.py
@@ -11,5 +11,13 @@
 #its describe the lenght of the string
 # if we print only write
 str = "mimansha pgjhgvt6ydfuytf"
-print(str[5:])
+print(str[5:])#[5:4] 
+#bcoz its know what the user wants to tell
+"""negative index"""
+ #we backward counting the str
+" -1 -2 -3 so on "
+str ="apple"
+print(str[-5:-4])
+
+
 
