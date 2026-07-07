dd3106535057e1218f824a0d95fa5a7bee4c5574
mimansha23 <mimanshapandey89@gmail.com>
2026-07-07 21:37:31 +0530

    addinng new text

--- slicing.py
+++ slicing.py
@@ -6,5 +6,10 @@
 # str[ :4] is same as str[0:4]
 # str[1:] is same as str[1:len(str)]
 
-str ="mimansha pandey"
-print(str[5: len(str)])
+# str ="mimansha pandey"
+# print(str[5: len(str)])
+#its describe the lenght of the string
+# if we print only write
+str = "mimansha pgjhgvt6ydfuytf"
+print(str[5:])
+
