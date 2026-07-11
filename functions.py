e66f5130cd2ce406de55e5a15e2913615658d1da
mimansha23 <mimanshapandey89@gmail.com>
2026-07-11 22:19:13 +0530

    replace function

--- functions.py
+++ functions.py
@@ -10,3 +10,9 @@ print(str.endswith("app"))
 """str.capitalize()"""
 # capitalizes 1st char
 
+"""str.replace(ols,new)"""
+str =" i am studying python from apna college"
+print(str.replace("o","a"))
+# its used to replace the word or letter 
+#like its print the value with change in the o wherever a is.
+#output is = i am studying pythan fram apna callege
