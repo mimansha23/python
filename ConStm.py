a9c01e23ea4c56206819c60190eb415032abec4d
mimansha23 <mimanshapandey89@gmail.com>
2026-07-18 14:37:43 +0530

    adding if conditional statement

---
+++ ConStm.py
@@ -0,0 +1,12 @@
+"""if-elif-else(syntax)"""
+#if(condition):
+"""statement
+elif(condition):
+statement2
+else(condition):
+statement3"""
+
+age = 24
+if(age >= 18):
+     print("canvote")
+     print("candrive")
