8d2b2c2098ea5c9ae66cab755e8b673ad73c8da0
mimansha23 <mimanshapandey89@gmail.com>
2026-07-19 23:54:20 +0530

    adding new else if condition

--- ConStm.py
+++ ConStm.py
@@ -5,8 +5,19 @@ elif(condition):
 statement2
 else(condition):
 statement3"""
-
+"""
 age = 24
 if(age >= 18):
      print("canvote")
-     print("candrive")
+     print("candrive")"""
+
+#else if 
+light ="greeen"
+    
+if(light == "red"):
+   print("stop")
+elif(light == "yellow"):
+   print("wait")
+elif(light == "green"):
+   print("go")
+
