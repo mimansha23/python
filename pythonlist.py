388033acff2ac762eafa4a89d69a94989a69d281
mimansha23 <mimanshapandey89@gmail.com>
2026-08-30 11:21:04 +0530

    print list with no return

--- pythonlist.py
+++ pythonlist.py
@@ -19,3 +19,4 @@ print(thistuple[2:5])"""
 """This example returns the items from the beginning to, but NOT included, "kiwi":"""
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[:4])
+
